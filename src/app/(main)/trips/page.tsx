3bfc62dda1b4a567a118fdc364ea5962c385ff9e
Yaseer A. Okino <yaseerokino@icloud.com>
2024-04-29 23:04:36 +0100

    temp fix build error

---
+++ src/app/(main)/trips/page.tsx
@@ -0,0 +1,27 @@
+import { getReservations } from "@/server/actions/reservation";
+
+import { getCurrentUser } from "@/lib/auth-helpers";
+import EmptyState from "@/components/empty-state";
+
+const TripsPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return <EmptyState title="Unauthorized, Please Login" />;
+  }
+
+  const reservations = await getReservations({
+    userId: currentUser.id,
+  });
+
+  if (reservations.length === 0) {
+    return (
+      <div>
+        <EmptyState
+          title="No Trips found"
+          subtitle="Looks like you haven't reserved any trips"
+        />
+      </div>
+    );
+  }
+};
